7b967221339c40803ede002af7c739c6370ca28a
raviakella03 <raviakella03@outlook.com>
2023-03-25 15:55:32 +0530

    Took the useState to a different component

--- src/components/newPost/NewPost.js
+++ src/components/newPost/NewPost.js
@@ -1,28 +1,27 @@
-import { useState } from 'react';
+import newPostStyles from "./NewPost.module.css";
 
-import newPostStyles from './NewPost.module.css';
-
-function NewPost() {
-
-    const [postContent, setPostContent] = useState('');
-
-    function changeBodyHandler(event) {
-        setPostContent(event.target.value);
-    }
-
-    return (
-        <form className={newPostStyles.form}>
-            <p>
-                <label htmlFor='postContent'>Text</label>
-                <textarea id="postContent" required rows={3} onChange={changeBodyHandler}/>
-            </p>
-            <p>{postContent}</p>
-            <p>
-                <label htmlFor='postAuthor'>Your Name</label>
-                <textarea id="postAuthor" required />
-            </p>
-        </form>
-    );
+function NewPost(props) {
+  return (
+    <form className={newPostStyles.form}>
+      <p>
+        <label htmlFor="postContent">Text</label>
+        <textarea
+          id="postContent"
+          required
+          rows={3}
+          onChange={props.onPostContentChange}
+        />
+      </p>
+      <p>
+        <label htmlFor="postAuthor">Your Name</label>
+        <textarea
+          id="postAuthor"
+          required
+          onChange={props.onPostAuthorChange}
+        />
+      </p>
+    </form>
+  );
 }
 
 export default NewPost;
