c2fdf1749d7db473ba14f5164c6bf140bb83b6ff
raviakella03 <raviakella03@outlook.com>
2023-03-26 00:30:53 +0530

    Added communication with backend server
    Added code to save a new post to backend
    Used useEffect in Posts component to load data from backend server.

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Modal from "../common/modal/Modal";
 import NewPost from "../newPost/NewPost";
 import Post from "./post/Post";
@@ -6,8 +6,34 @@ import postsStyles from "./Posts.module.css";
 
 function Posts(props) {
   const [posts, setPosts] = useState([]);
+  const [isFetchingPosts, setIsFetchingPosts] = useState(false);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetchingPosts(true);
+      //use localhost for development.
+      //use ip to access on multiple devices on same network like mobile/ipad at home
+      // const response = await fetch("http://localhost:8085/posts");
+      const response = await fetch("http://192.168.1.102:8085/posts");
+      const responseData = await response.json();
+      setPosts(responseData.posts);
+      setIsFetchingPosts(false);
+    }
+
+    fetchPosts();
+  }, []);
 
   function addNewPostHandler(postData) {
+    //Posting data to backend server.
+    // fetch() is used for both fetching and posting data from/to server
+    //use localhost for development.
+    //use ip to access on multiple devices on same network like mobile/ipad at home
+    // fetch("http://localhost:8085/posts", {
+    fetch("http://192.168.1.102:8085/posts", {
+      method: "Post",
+      body: JSON.stringify(postData),
+      headers: { "Content-Type": "application/json" },
+    });
     // setPosts([postData, ...posts]); //or
     setPosts((existingPosts) => [postData, ...posts]);
   }
@@ -30,7 +56,7 @@ function Posts(props) {
           </Modal>
         ) */
       }
-      {posts.length > 0 && (
+      {!isFetchingPosts && posts.length > 0 && (
         <ul className={postsStyles.posts}>
           {posts.map((post, index) => (
             <Post
@@ -41,10 +67,15 @@ function Posts(props) {
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetchingPosts && posts.length === 0 && (
         <div className={postsStyles.noPosts}>
           <h2>No Posts yet!</h2>
           <p>Start adding some posts</p>
+        </div>
+      )}
+      {isFetchingPosts && (
+        <div className={postsStyles.loadingPosts}>
+          <h2>Loading posts...</h2>
         </div>
       )}
     </React.Fragment>
